7982336318fee3b0016b1894b298be5bfd5d35d8
Megumi912 <94968677+Megumi912@users.noreply.github.com>
2022-01-13 00:46:24 +0900

    指摘された箇所の修正

--- script.js
+++ script.js
@@ -8,12 +8,16 @@
     function toggleNav() {
     var body = document.body;
     var hamburger = document.getElementById('js-hamburger');
-    var blackBg = document.getElementById('c-hamburger_line_btn');
+    var hamburgerBt = document.getElementById('c-hamburger_line_btn');
+    var blackbg = document.getElementById('p-sidebar_bg');
 
     hamburger.addEventListener('click', function() {
       body.classList.toggle('nav_open'); //メニュークリックでnav-openというクラスがbodyに付与
     });
-    blackBg.addEventListener('click', function() {
+    hamburgerBt.addEventListener('click', function() {
+      body.classList.remove('nav_open'); //もう一度クリックで解除
+    });
+    blackbg.addEventListener('click', function() {
       body.classList.remove('nav_open'); //もう一度クリックで解除
     });
 }
